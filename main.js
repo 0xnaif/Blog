4bd31a68641396dd59b60b6f2cbbbb67199b7e97
0xnaif <niefm1422@gmail.com>
2024-04-01 17:48:49 +0300

    Add initial files

---
+++ main.js
@@ -0,0 +1,75 @@
+function showUserProf() {
+    let userInfo_el = document.getElementsByClassName("info")[0];
+    let overlay = document.getElementById("overlay");
+    
+    overlay.setAttribute("style", "display: block");
+    userInfo_el.setAttribute("style", "display: flex");
+}
+
+function showEmail(element) {
+    let back_el = document.getElementsByClassName("back")[0];
+    let email_info = document.getElementsByClassName("email")[0];
+    let email_el = element;
+    let password_el = document.getElementsByClassName("password-choice")[0];
+    let logout_el = document.getElementsByClassName("log-out")[0];
+    
+    back_el.setAttribute("style", "display: flex");
+    email_el.setAttribute("style", "display: none");
+    password_el.setAttribute("style", "display: none");
+    logout_el.setAttribute("style", "display: none");
+    email_info.setAttribute("style", "display: flex");
+}
+
+function back(element) {
+    let back_el = element;
+    let email_info = document.getElementsByClassName("email")[0];
+    let email_el = document.getElementsByClassName("email-choice")[0];
+    let password_el = document.getElementsByClassName("password-choice")[0];
+    let logout_el = document.getElementsByClassName("log-out")[0];
+    
+    email_info.setAttribute("style", "display: none");
+    back_el.setAttribute("style", "display: none");
+    email_el.setAttribute("style", "display: flex");
+    password_el.setAttribute("style", "display: flex");
+    logout_el.setAttribute("style", "display: flex");
+}
+
+function hideUserProf() {
+    let userInfo_el = document.getElementsByClassName("info")[0];
+    let overlay = document.getElementById("overlay");
+    let back_el = document.getElementsByClassName("back")[0];
+    let email_info = document.getElementsByClassName("email")[0];
+    let email_el = document.getElementsByClassName("email-choice")[0];
+    let password_el = document.getElementsByClassName("password-choice")[0];
+    let logout_el = document.getElementsByClassName("log-out")[0];
+    
+    userInfo_el.setAttribute("style", "display: none");
+    overlay.setAttribute("style", "display: none");
+    email_info.setAttribute("style", "display: none");
+    back_el.setAttribute("style", "display: none");
+    email_el.setAttribute("style", "display: flex");
+    password_el.setAttribute("style", "display: flex");
+    logout_el.setAttribute("style", "display: flex");
+}
+
+function showConfirm(className) {
+    let confirm_el = document.getElementsByClassName(className)[0];
+    confirm_el.setAttribute("style", "display: flex");
+}
+
+function hideConfirm(className) {
+    let confirm_el = document.getElementsByClassName(className)[0];
+    confirm_el.setAttribute("style", "display: none");
+}
+
+const url = document.URL;
+
+if (url.includes("posts")) {
+    let deleteIcns = document.querySelectorAll(".main .posts i");
+    for (let i =  0; i < deleteIcns.length; i++) {
+        deleteIcns[i].addEventListener("click", () => {
+            var postEl = deleteIcns[i].parentElement;
+            postEl.remove();
+        })
+    }
+}
