6268134cdb90b72a271c1652d24979b60f90c0be
0xnaif <niefm1422@gmail.com>
2024-04-08 18:24:59 +0300

    Valid sign-in/up inputs

--- server.js
+++ server.js
@@ -15,10 +15,36 @@
         res.render("sign-up");
     });
 
+    app.post("/signup", (req, res) => {
+        const { email, password, confirmPassword } = req.body
+        if (email == "" || password == "" || confirmPassword == "")
+            res.render("sign-up", {error: "Try again, incomplete fields"});
+
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email) || password.length < 8)
+            res.render("sign-up", {error: "Try again, incorrect email or password"});
+
+        if (password != confirmPassword)
+            res.render("sign-up", {error: "Try again, Confirm password doesn't match"});
+
+        res.redirect("/");
+    });
+
     app.get("/signin", (req, res) => {
         res.render("sign-in");
     });
 
+    app.post("/signin", (req, res) => {
+        const { email, password } = req.body
+        if (email == "" || password == "")
+            res.render("sign-in", {error: "Try again, incomplete fields"});
+
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email) || password.length < 8)
+            res.render("sign-in", {error: "Try again, incorrect email or password"});
+
+        res.redirect("/");
+    })
     app.get("/about", (req, res) => {
         res.render("about");
     });
