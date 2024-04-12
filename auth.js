8b21f14c4ee88d9dff0bc38b13e458cc5d1a5ac7
0xnaif <niefm1422@gmail.com>
2024-04-12 16:01:50 +0300

    Complete sign-in/up functions

--- auth.js
+++ auth.js
@@ -5,35 +5,48 @@ import crypto from "crypto";
 const saltRounds = 10;
 const secretKey = crypto.randomBytes(64).toString('hex');
 
-export function signup(req, res) {
-    const { email, password, confirmPassword } = req.body
-    if (email == "" || password == "" || confirmPassword == "")
-        return res.render("sign-up", {error: "Try again, incomplete fields"});
+export async function signup(req, res, db) {
+    try {
+        const { email, password, confirmPassword } = req.body
+        if (email == "" || password == "" || confirmPassword == "")
+            return res.render("sign-up", {error: "Try again, incomplete fields"});
+    
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email) || password.length < 8)
+            return res.render("sign-up", {error: "Try again, incorrect email or password"});
+    
+        if (password != confirmPassword)
+            return res.render("sign-up", {error: "Try again, confirm password doesn't match"});
 
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailRegex.test(email) || password.length < 8)
-        return res.render("sign-up", {error: "Try again, incorrect email or password"});
+        const result = await db.searchUser(email);
+        if (result.rows.length > 0)
+            return res.render("sign-up", {error: "User already exists"});
 
-    if (password != confirmPassword)
-        return res.render("sign-up", {error: "Try again, Confirm password doesn't match"});
-
-    bcrypt.hash(password, saltRounds, (err, hash) => {
-        if (err)
-            return res.status(500).send("Internal server error");
-
-        const token = jwt.sign({id : 1, email : email}, secretKey, {expiresIn : "1m"});
-        console.log("hashed passwrd: ", hash);
-        console.log("token", token);
-        res.cookie("jwtToken", token, {
-            maxAge : 60000,
-            httpOnly: true,
-            secure: false,
-        });
-        res.redirect("/");
-    });
+        else {
+            bcrypt.hash(password, saltRounds, async (err, hash) => {
+                if (err) {
+                    console.error("An error occurred during hashing: ", err);
+                    return res.status(500).send("Internal server error");
+                }        
+                const data = ["Test", email, hash];
+                const result = await db.addNewUser(data);
+                const token = jwt.sign({id : result.id, email : email}, secretKey, {expiresIn : "1m"});
+                res.cookie("jwtToken", token, {
+                    maxAge : 60000,
+                    httpOnly: true,
+                    secure: false,
+                });
+                res.redirect("/");
+            });
+        }
+        }
+    catch (err) {
+        console.error("An error occurred during sign up: ", err);
+        res.status(500).send("Internal server error")
+    }
 }
 
-export async function signin(req, res) {
+export async function signin(req, res, db) {
     const { email, password } = req.body
     if (email == "" || password == "")
         return res.render("sign-in", {error: "Try again, incomplete fields"});
@@ -42,18 +55,28 @@ export async function signin(req, res) {
     if (!emailRegex.test(email) || password.length < 8)
         return res.render("sign-in", {error: "Try again, incorrect email or password"});
 
-    bcrypt.hash(password, saltRounds, (err, hash) => {
-        if (err)
-            return res.status(500).send("Internal server error");
+
+    const result = await db.searchUser(email);
+    if (result.rows.length == 0)
+        return res.render("sign-in", {error: "User not found"});
+
+    else {
+        const user = result.rows[0];
+        const storedPassword = user.password;
         
-        const token = jwt.sign({id : 1, email : email}, secretKey, {expiresIn : "2m"});
-        res.cookie("jwtToken", token, {
-            maxAge : 120000,
-            httpOnly: true,
-            secure: false,
-        });
-        res.redirect("/");
-    });
+        bcrypt.compare(password, storedPassword, (err, result) => {
+            if (err || !result)
+                return res.render("sign-in", {error: "Try again, incorrect email or password"});
+
+            const token = jwt.sign({id : user.id, email : email}, secretKey, {expiresIn : "2m"});
+            res.cookie("jwtToken", token, {
+                maxAge : 120000,
+                httpOnly: true,
+                secure: false,
+            });
+            res.redirect("/");
+        })
+    }
 }
 
 function verify(token) {
