a37639b2721d8ec1de3be611675549616f9ec905
0xnaif <niefm1422@gmail.com>
2024-07-09 08:32:32 +0300

    Implement back button function

--- public/edit-post.js
+++ public/edit-post.js
@@ -1,43 +1,47 @@
-import { response } from "express";
-
 document.addEventListener("DOMContentLoaded", () => {
     const form = document.getElementById("edit-form");
     form.addEventListener("submit", async (e) => {
         try {
             e.preventDefault();
-            const formData = new FormData(form);
-            const data = Object.fromEntries(formData.entries());
-            const postID = document.querySelector(".main .post").id;
-            const response = await fetch(`/edit-post?id=${postID}`, {
-                method : "PUT",
-                headers : {
-                    "Content-Type" : "application/json",
-                },
-                body : JSON.stringify(data),
-            });
-            if (response.ok) {
-                const res = await response.json();
-                document.getElementById("response").style.setProperty('--beforeBackgroundColor', "#63E6BE");
-                document.getElementById("content").style.color = "#63E6BE";
-                document.querySelector(".main #response i").style.color = "#63E6BE";
-                document.getElementById("response").style.display = "flex";
-                document.getElementById("content").innerText = res.message;
-                setTimeout(() => {
-                    document.getElementById("response").style.display = "none";
-                    window.location.reload();
-                }, 600);
+            if (e.submitter.id == "backButton") {
+                window.history.back();
             }
             else {
-                const res = await response.json();
-                document.getElementById("response").style.setProperty('--beforeBackgroundColor', "#F00");
-                document.getElementById("content").style.color = "#F00";
-                document.querySelector(".main #response i").style.color = "#F00";
-                document.getElementById("response").style.display = "flex";
-                document.getElementById("content").innerText = res.message;
-                setTimeout(() => {
-                    document.getElementById("response").style.display = "none";
-                    window.location.reload();
-                }, 600);
+                const formData = new FormData(form);
+                const data = Object.fromEntries(formData.entries());
+                const postID = document.querySelector(".main .post").id;
+                const response = await fetch(`/edit-post?id=${postID}`, {
+                    method : "PUT",
+                    headers : {
+                        "Content-Type" : "application/json",
+                    },
+                    body : JSON.stringify(data),
+                });
+                if (response.ok) {
+                    const res = await response.json();
+                    document.getElementById("response").style.setProperty('--beforeBackgroundColor', "#63E6BE");
+                    document.getElementById("content").style.color = "#63E6BE";
+                    document.querySelector(".main #response i").style.color = "#63E6BE";
+                    document.getElementById("response").style.display = "flex";
+                    document.getElementById("content").innerText = res.message;
+                    console.log(res.message);
+                    setTimeout(() => {
+                        document.getElementById("response").style.display = "none";
+                        window.location.href = "/posts";
+                    }, 600);
+                }
+                else {
+                    const res = await response.json();
+                    document.getElementById("response").style.setProperty('--beforeBackgroundColor', "#F00");
+                    document.getElementById("content").style.color = "#F00";
+                    document.querySelector(".main #response i").style.color = "#F00";
+                    document.getElementById("response").style.display = "flex";
+                    document.getElementById("content").innerText = res.message;
+                    setTimeout(() => {
+                        document.getElementById("response").style.display = "none";
+                        window.location.href = "/posts";
+                    }, 600);
+                }
             }
         }
         catch(err) {
