7c226d62ddd55c75f30e02e1bb7cfae290aea972
0xnaif <niefm1422@gmail.com>
2024-07-28 12:34:06 +0300

    Update

--- public/new-post.js
+++ public/new-post.js
@@ -1,26 +1,39 @@
-const resElement = document.getElementById("response");
-const state = resElement.dataset.state;
-const type = resElement.dataset.type;
-
 document.addEventListener("DOMContentLoaded", () => {
-    if (state) {
-        showPopUp(type);
-    }
+    const form = document.getElementById("new-post");
+    form.addEventListener("submit", async (e) => {
+        e.preventDefault();
+        const title = document.getElementById("title").value;
+        const content = document.getElementById("postContent").value;
+        if (title.length == 0 || content.length == 0) {
+            document.getElementById("content").innerText = "Please complete all empty fields";
+            document.getElementById("response").style.display = "flex";
+            setTimeout(() => {
+                document.getElementById("response").style.display = "none";
+            }, 3000)
+        }
+        else {
+            const response = await fetch("/new-post", {
+                method : "POST", 
+                headers : {
+                    "Content-Type" : "application/json",
+                },
+                body : JSON.stringify({ title : title, content : content }),
+            });
+            if (response.ok) {
+                const res = await response.json();
+                document.getElementById("response").style.setProperty('--beforeBackgroundColor', "#63E6BE");
+                document.getElementById("content").style.color = "#63E6BE";
+                document.querySelector("#response i").style.color = "#63E6BE";
+                document.getElementById("content").innerText = res.message;
+                document.getElementById("response").style.display = "flex";
+                setTimeout(() => {
+                    document.getElementById("response").style.display = "none";
+                    window.location.href = "/posts";
+                }, 3000)
+            }
+            else {
+                window.location.href = "/signin";
+            }
+        }
+    })
 });
-
-function showPopUp(type) {
-    if (type === "success") {
-        resElement.style.setProperty('--beforeBackgroundColor', "#63E6BE");
-        
-        setTimeout(() => {
-            window.location.href = "/";
-        }, 600);
-    }
-    
-    else if (type === "error") {
-        resElement.style.setProperty('--beforeBackgroundColor', '#F00'); 
-        setTimeout(() => {
-            resElement.style.display = "none";
-        }, 600);
-    }
-}
