9a5c190112e8ad66a6822a5392e3365623e8ed0f
0xnaif <niefm1422@gmail.com>
2024-08-26 20:17:12 +0300

    Update code to conform with rich text editor & Update empty fields handle

--- public/edit-post.js
+++ public/edit-post.js
@@ -7,40 +7,52 @@ document.addEventListener("DOMContentLoaded", () => {
                 window.history.back();
             }
             else {
-                const formData = new FormData(form);
-                const data = Object.fromEntries(formData.entries());
-                const postID = document.querySelector(".main .post").id;
-                const response = await fetch(`/edit-post?id=${postID}`, {
-                    method : "PUT",
-                    headers : {
-                        "Content-Type" : "application/json",
-                    },
-                    body : JSON.stringify(data),
-                });
-                if (response.ok) {
-                    const res = await response.json();
-                    document.getElementById("response").style.setProperty('--beforeBackgroundColor', "#63E6BE");
-                    document.getElementById("content").style.color = "#63E6BE";
-                    document.querySelector("#response i").style.color = "#63E6BE";
+                const title = document.getElementById("title").value;
+                const content = document.getElementById("editor");
+                const data = {
+                    title : title,
+                    content : content.innerHTML,
+                };
+                if (title.length == 0 || content.textContent.length == 0) {
+                    document.getElementById("content").innerText = "Please complete all empty fields";
                     document.getElementById("response").style.display = "flex";
-                    document.getElementById("content").innerText = res.message;
-                    console.log(res.message);
                     setTimeout(() => {
                         document.getElementById("response").style.display = "none";
-                        window.location.href = "/posts";
-                    }, 3000);
+                    }, 3000)
                 }
                 else {
-                    const res = await response.json();
-                    document.getElementById("response").style.setProperty('--beforeBackgroundColor', "#F00");
-                    document.getElementById("content").style.color = "#F00";
-                    document.querySelector("#response i").style.color = "#F00";
-                    document.getElementById("response").style.display = "flex";
-                    document.getElementById("content").innerText = res.message;
-                    setTimeout(() => {
-                        document.getElementById("response").style.display = "none";
-                        window.location.href = "/posts";
-                    }, 3000);
+                    const postID = document.querySelector(".main .post").id;
+                    const response = await fetch(`/edit-post?id=${postID}`, {
+                        method : "PUT",
+                        headers : {
+                            "Content-Type" : "application/json",
+                        },
+                        body : JSON.stringify(data),
+                    });
+                    if (response.ok) {
+                        const res = await response.json();
+                        document.getElementById("response").style.setProperty('--beforeBackgroundColor', "#63E6BE");
+                        document.getElementById("content").style.color = "#63E6BE";
+                        document.querySelector("#response i").style.color = "#63E6BE";
+                        document.getElementById("response").style.display = "flex";
+                        document.getElementById("content").innerText = res.message;
+                        setTimeout(() => {
+                            document.getElementById("response").style.display = "none";
+                            window.location.href = "/posts";
+                        }, 3000);
+                    }
+                    else {
+                        const res = await response.json();
+                        document.getElementById("response").style.setProperty('--beforeBackgroundColor', "#F00");
+                        document.getElementById("content").style.color = "#F00";
+                        document.querySelector("#response i").style.color = "#F00";
+                        document.getElementById("response").style.display = "flex";
+                        document.getElementById("content").innerText = res.message;
+                        setTimeout(() => {
+                            document.getElementById("response").style.display = "none";
+                            window.location.href = "/posts";
+                        }, 3000);
+                    }
                 }
             }
         }
@@ -50,3 +62,23 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     })
 });
+
+function applyCommand(command) {
+    document.execCommand(command, false, null);
+    
+}
+
+const editor = document.getElementById("editor");
+const editorContainer = document.getElementById("editorContainer");
+
+editor.addEventListener("focus", () => {
+    editorContainer.classList.add("focused");
+});
+
+editor.addEventListener("blur", () => {
+    editorContainer.classList.remove("focused");
+});
+
+function changeFontSize(size) {
+    document.execCommand("fontSize", false, size);
+}
